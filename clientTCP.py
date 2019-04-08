feacef45e98515e2a6a441f47e058676c1547284
Gabriel de Matos Oliveira Fernandes <33075528+GabrieldMatos@users.noreply.github.com>
2019-04-08 19:22:00 -0300

    Update Host and Port
    host: ibiza.dcc.ufla.br
    port: 5008

--- clientTCP.py
+++ clientTCP.py
@@ -4,10 +4,10 @@ import time
   
 def Main(): 
 	# host IP
-	host = '127.0.0.1'
+	host = 'ibiza.dcc.ufla.br'
   
 	# Define the port on which you want to connect 
-	port = 12345
+	port = 5008
   
 	s = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
 	# connect to server
